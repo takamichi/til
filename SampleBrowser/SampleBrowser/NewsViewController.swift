c4f45ea16bd08173282116b4bddd3aa16abb0c36
Takamichi Urata <taka@seraphimis.net>
2018-07-09 23:21:21 +0900

    現場のためのSwift4 P.367-P.401

---
+++ SampleBrowser/SampleBrowser/NewsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewsViewController.swift
+//  SampleBrowser
+//
+//  Created by Takamichi URATA on 2018/07/09.
+//  Copyright © 2018年 Takamichi URATA. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
