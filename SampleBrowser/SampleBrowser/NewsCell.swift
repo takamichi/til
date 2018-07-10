ce1af4e47fe990ae8944ed79b26d8612ea4f5011
Takamichi Urata <taka@seraphimis.net>
2018-07-10 23:52:46 +0900

    現場のためのSwift4 P.402-P.410

---
+++ SampleBrowser/SampleBrowser/NewsCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsCell.swift
+//  SampleBrowser
+//
+//  Created by Takamichi URATA on 2018/07/10.
+//  Copyright © 2018年 Takamichi URATA. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
