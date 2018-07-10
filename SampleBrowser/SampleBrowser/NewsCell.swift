0661623cb4b904d18b6efc5770e05545ff70fcb0
Takamichi Urata <taka@seraphimis.net>
2018-07-11 00:12:01 +0900

    現場のためのSwift4 P.410

--- SampleBrowser/SampleBrowser/NewsCell.swift
+++ SampleBrowser/SampleBrowser/NewsCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class NewsCell: UITableViewCell {
 
+    @IBOutlet weak var dateLabel: UILabel!
+
+    @IBOutlet weak var titleLabel: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +24,5 @@ class NewsCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    
+
 }
