e018280fd6f6fcf39b77f818d7023974fe9c7673
Takamichi Urata <taka@seraphimis.net>
2018-08-01 08:37:10 +0900

    Udemy ios11basics: レクチャー23まで (検索と地図にピン立て)

--- Udemy ios11basics/MyMap/MyMap/ViewController.swift
+++ Udemy ios11basics/MyMap/MyMap/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var dispayMap: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        inputText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを閉じる
+        textField.resignFirstResponder()
 
+        let keyword = textField.text;
+        print(keyword!)
+
+        // キーワードから緯度・経度の検索し、結果のピンを表示する
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(
+            keyword!,
+            completionHandler: {
+                (placemarks: [CLPlacemark]?, error: Error?) in
+                if let placemark = placemarks?[0] {
+                    if let targetCordinate = placemark.location?.coordinate {
+                        print(targetCordinate)
+
+                        // ピンを作成
+                        let pin = MKPointAnnotation()
+                        pin.coordinate = targetCordinate
+                        pin.title = keyword
+
+                        // ピンの追加と表示範囲の調整
+                        self.dispayMap.addAnnotation(pin)
+                        self.dispayMap.region = MKCoordinateRegionMakeWithDistance(targetCordinate, 500.0, 500.0)
+                    }
+                }
+            }
+        )
+
+        return true
+    }
 }
-
