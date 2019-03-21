5b60b0e4389f0b5c0c3d9b23da9fb5b50233bab2
Takamichi Urata <taka@seraphimis.net>
2019-03-21 16:58:28 +0900

    Udemy iOS12: セクション1.
    【iOS12対応】未経験者が有名アプリ開発者になるiOS 12の全て 20個以上アプリをつくりプロになる | Udemy
    https://www.udemy.com/ios12_swift5_iphonex/learn/v4/overview

---
+++ Udemy ios12_swift5_iphonex/SwiftStart/SwiftStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftStart
+//
+//  Created by Takamichi URATA on 2019/03/21.
+//  Copyright © 2019 Takamichi URATA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
